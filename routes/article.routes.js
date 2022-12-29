2b907be40a45f7893d322e1a9340631743ffc4bd
valdidev <iamvaldidev@gmail.com>
2022-12-29 20:48:36 +0100

    chore: move comments

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -4,12 +4,12 @@ const { addArticleController } = require('../controllers/article.controllers');
 
 router.post('/add', addArticleController);
 // mvp
-// router.get('/all', (req, res) => {res.send('ver todos los artículos')});
 router.put('/modify', (req, res) => {res.send('modificar un artículo')});
 router.delete('/delete', (req, res) => {res.send('borrar un artículo')});
 router.patch('/visibility', (req, res) => {res.send('modificar visibilidad artículo')});
 
 // deluxe
+// router.get('/all', (req, res) => {res.send('ver todos los artículos')});
 // router.get('/name', (req, res) => {res.send('busqueda por nombre')});
 // router.get('/category/all', (req, res) => {res.send('ver artículos de una categoría')});
 // router.get('/brand/all', (req, res) => {res.send('ver artículos de una marca')});
