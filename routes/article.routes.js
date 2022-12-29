84fef3b29f5b2960c0cfa20183863edc5c31f677
valdidev <iamvaldidev@gmail.com>
2022-12-29 20:43:01 +0100

    wip: next endpoints

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -3,5 +3,15 @@ const { addArticleController } = require('../controllers/article.controllers');
 
 
 router.post('/add', addArticleController);
+// mvp
+// router.get('/all', (req, res) => {res.send('ver todos los artículos')});
+router.put('/modify', (req, res) => {res.send('modificar un artículo')});
+router.delete('/delete', (req, res) => {res.send('borrar un artículo')});
+router.patch('/visibility', (req, res) => {res.send('modificar visibilidad artículo')});
+
+// deluxe
+// router.get('/name', (req, res) => {res.send('busqueda por nombre')});
+// router.get('/category/all', (req, res) => {res.send('ver artículos de una categoría')});
+// router.get('/brand/all', (req, res) => {res.send('ver artículos de una marca')});
 
 module.exports = router;
