a1a7414249bd113bf308fdcaa9c4e2c03938ea22
valdidev <iamvaldidev@gmail.com>
2022-12-31 12:38:56 +0100

    chore: change salt

---
+++ seeders/03-demo-article.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.bulkDelete('People', null, {});
+    
+  }
+};
