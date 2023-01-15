62f8c97839d59cf9b9b8d6e67ecd4c0808125f18
valdidev <iamvaldidev@gmail.com>
2023-01-15 09:27:08 +0100

    deploy: sequelize index models, config.json to .js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "port": process.env.DB_PORT,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
